3bf0c3ffd03ae25ff5c765e164680b39ed80273e
ujjwalmishra2050 <ujjwalmishra272@gmail.com>
2025-05-15 00:31:44 +0545

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import tailwindcss from '@tailwindcss/vite'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
